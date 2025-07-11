0c724282e70e9cc7951d60438f60c87a46f8e822
Arman Martirosyan <arman.martirosyan@nrgaming.com>
2025-07-11 14:53:15 +0400

    Changed: node-redis to ioredis

---
+++ src/core/redis/redis.service.ts
@@ -0,0 +1,47 @@
+import { Redis, type RedisKey, type RedisValue } from "ioredis";
+
+import { configService } from "../../config";
+import { logger } from "../../common/logger";
+
+class RedisService {
+  private readonly _client: Redis;
+
+  private constructor() {
+    this._client = new Redis(configService.env.REDIS_URL, {
+      retryStrategy: (times: number): number | null => {
+        if (times > 3)
+          return null;
+        return Math.min(times * 1000, 2000);
+      },
+    });
+
+    this._client.on("connect", () => logger.info("Redis connected."));
+    this._client.on("close", () => logger.info("Redis disconnected."));
+    this._client.on("error", (err: any) => logger.error("Redis Client Error:", err));
+  }
+
+  static create(): RedisService {
+    return new RedisService();
+  }
+
+  public disconnect(): void {
+    this._client.disconnect();
+  }
+
+  public async set(key: RedisKey, value: RedisValue, expiryInSeconds?: number): Promise<"OK"> {
+    if (expiryInSeconds)
+      return this._client.set(key, value, "EX", expiryInSeconds);
+    return this._client.set(key, value);
+  }
+
+  public async get(key: RedisKey): Promise<string | null> {
+    return this._client.get(key);
+  }
+
+  public del(key: RedisKey): Promise<number> {
+    return this._client.del(key);
+  }
+}
+
+const redis: RedisService = RedisService.create();
+export { redis };
